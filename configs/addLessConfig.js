bfb2664fa7b9e17322cedb9425a8bcadfcc7a42c
caowenjie <caowj@finchina.local>
2022-09-19 14:32:29 +0800

    优化工具

--- configs/addLessConfig.js
+++ configs/addLessConfig.js
@@ -3,22 +3,22 @@ const addLessLoader = require("customize-cra-less-loader");
 const addLessConfig = () => {
   return [
     addLessLoader({
-        lessLoaderOptions: {
-          lessOptions: {
-            strictMath: true,
-            noIeCompat: true,
-            modifyVars: {
-              '@primary-color': "#1DA57A",
-            },
-            cssModules: {
-              localIdentName:
-                process.env.NODE_ENV === 'development'
-                  ? '[path][name]__[local]--[hash:base64:5]'
-                  : '[sha512:hash:base64:7]',
-            },
+      lessLoaderOptions: {
+        lessOptions: {
+          javascriptEnabled: true,
+          math: "always",
+          modifyVars: {
+            "@primary-color": "#1DA57A",
+          },
+          cssModules: {
+            localIdentName:
+              process.env.NODE_ENV === "development"
+                ? "[path][name]__[local]--[hash:base64:5]"
+                : "[sha512:hash:base64:7]",
           },
         },
-      }),
+      },
+    }),
   ];
 };
 
