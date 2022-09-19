f8da4540f070ffc6da895919070738be20d5c0e9
caowenjie <caowj@finchina.local>
2022-09-19 10:36:26 +0800

    ts声明

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,4 +1,12 @@
 /// <reference types="react-scripts" />
+
+declare module '*.css';
+declare module '*.png';
+declare module '*.jpg';
+
+declare interface Window {
+  echarts: any;
+}
 declare module "*.less" {
   const classes: { readonly [key: string]: string };
   export = classes;
